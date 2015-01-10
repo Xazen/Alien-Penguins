0c0b0e288f1c7a84c1092df7242123cabc71c0b7
Dennis Lieu <dennis.lieu@xazen.de>
2015-01-10 15:56:37 +0100

    - Add basic rotation script

--- JamProjectJanuary/Assets/Scripts/PlayerControl.cs
+++ JamProjectJanuary/Assets/Scripts/PlayerControl.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	[SerializeField] private AnimationCurve acceleration;
+	[SerializeField] private Vector3 eulerAngleVelocity = new Vector3(0, 100, 0);
+
+	private Rigidbody playerRigidbody;
 
 	// Use this for initialization
 	void Start () {
-	
+		playerRigidbody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float horizontalMovement = Input.GetAxis ("Horizontal"));
+	void FixedUpdate () {
+		float horizontalMovement = Input.GetAxis("Horizontal");
+
+		Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime * horizontalMovement);
+		rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
 
 		
-	
+
 	}
 }
