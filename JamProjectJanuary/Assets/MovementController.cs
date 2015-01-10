112e45596f8ee9a6187e9f9a3e2af20b28c224c3
Viktor Pekar <vikpek@gmail.com>
2015-01-10 14:45:10 +0100

    first working platform

---
+++ JamProjectJanuary/Assets/MovementController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+	
+	[SerializeField]	
+	private float movementSpeed = 5;
+	
+	private Transform _transform;
+	
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		
+		_transform.localPosition += input * Time.deltaTime * movementSpeed;
+	}
+}
---
+++ JamProjectJanuary/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Leap;
+
+
+
+public class PlatformController : MonoBehaviour {
+		
+		//	GameObject shipController;
+		
+		
+		Vector position;
+		Hand hand;
+		
+		Frame frame;
+		
+		[SerializeField]
+		Transform platformTransform;
+		
+		GameObject platformBasePosition;
+		GameObject platformController;
+		
+		Vector3 lastPosition;
+		
+		float miceMoveY = 0.0f;
+		
+		[SerializeField]
+		float sensitivityY = 0.005f;
+		
+		float miceMoveX = 0.0f;
+		
+		[SerializeField]
+		float sensitivityX = 0.005f;
+		
+		static Controller controller = new Controller();
+		
+		
+		void Start () {
+//			platformBasePosition = GameObject.Find("PlayerBasePosition");
+			platformController = GameObject.Find("PlatformController");
+		}
+		
+		
+		void Update () {			
+			UpdateLeapMotionPosition();
+//			UpdateMicePosition();
+		}
+		
+	// HACK who needs a mouse when you can have leap doo'h
+//		void UpdateMicePosition (){
+//			
+//			Transform tmpPos = shipTransform;
+//			
+//			miceMoveY += Input.GetAxis("Mouse Y") * sensitivityY;
+//			if (Input.GetAxis("Mouse Y") > 0){
+//				tmpPos.Translate(Vector3.up * miceMoveY);
+//			}
+//			if (Input.GetAxis("Mouse Y") < 0){
+//				tmpPos.Translate(Vector3.up * miceMoveY);
+//			}
+//			
+//			miceMoveX += Input.GetAxis("Mouse X") * sensitivityX;
+//			if (Input.GetAxis("Mouse X") > 0){
+//				tmpPos.Translate(Vector3.right * miceMoveX);
+//			}
+//			if (Input.GetAxis("Mouse X") < 0){
+//				tmpPos.Translate(Vector3.right * miceMoveX);
+//			}
+//			
+//			shipTransform.position = tmpPos.position;
+//		}
+//		
+		void UpdateLeapMotionPosition(){
+			controller = new Controller ();
+			
+			Frame frame = controller.Frame ();
+			HandList hands = frame.Hands;
+			Hand leapHand = hands[0];
+			PointableList pointables = frame.Pointables;
+			FingerList fingers = frame.Fingers;
+			ToolList tools = frame.Tools;
+			
+			if (leapHand == null){
+				platformTransform.localPosition = lastPosition;
+			} 
+			else 
+			{
+				Quaternion direction = leapHand.Fingers[1].Bone(Bone.BoneType.TYPE_DISTAL).Basis.Rotation();		
+				Quaternion directionCorrected = direction * Quaternion.Euler(35, 0, 0); 
+				
+				platformTransform.localPosition = platformController.transform.TransformPoint(leapHand.PalmPosition.ToUnityScaled());
+				
+				directionCorrected.x = 0f;
+				directionCorrected.y = 0f;
+				
+				platformTransform.rotation = directionCorrected;
+				lastPosition = platformTransform.localPosition;
+				
+			}
+		}
+	}
