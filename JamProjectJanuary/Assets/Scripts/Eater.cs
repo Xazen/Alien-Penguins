8dcb000b853f34cd429c0fc03b93d93ad588b394
Dennis Lieu <dennis.lieu@xazen.de>
2015-01-10 16:42:35 +0100

    - Penguin Eats Astronauts!

---
+++ JamProjectJanuary/Assets/Scripts/Eater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eater : MonoBehaviour {
+
+	private PlayerControl playerControl;
+
+	// Use this for initialization
+	void Start () {
+		GetComponentInParent<PlayerControl> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("Eater collider");
+		if (other.gameObject.tag == ("Astronaut")) 
+		{
+			Object.Destroy(other.gameObject);
+		}
+	}
+}
--- JamProjectJanuary/Assets/Scripts/PlayerControl.cs
+++ JamProjectJanuary/Assets/Scripts/PlayerControl.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
-	[SerializeField] private AnimationCurve acceleration;
+	[SerializeField] private AnimationCurve rotationAcceleration;
 	[SerializeField] private Vector3 eulerAngleVelocity = new Vector3(0, 100, 0);
 
+	private float time = 0;
 	private Rigidbody playerRigidbody;
+
+	void Awake () {
+		if (rotationAcceleration.keys.Length < 1) 
+		{
+			Debug.LogWarning("Transition Path not configured");
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -15,12 +23,14 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		time += Time.deltaTime;
 		float horizontalMovement = Input.GetAxis("Horizontal");
+		if (horizontalMovement == 0) 
+		{
+			time = 0;
+		}
 
-		Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime * horizontalMovement);
+		Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime * horizontalMovement * rotationAcceleration.Evaluate(time));
 		rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
-
-		
-
 	}
 }
