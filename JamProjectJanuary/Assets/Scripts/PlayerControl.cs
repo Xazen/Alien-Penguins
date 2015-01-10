464ae4e600fe1fd2ce38835291dd2623292c4ba5
Dennis Lieu <dennis.lieu@xazen.de>
2015-01-10 14:37:24 +0100

    - Start movement

---
+++ JamProjectJanuary/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	[SerializeField] private AnimationCurve acceleration;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalMovement = Input.GetAxis ("Horizontal"));
+
+		
+	
+	}
+}
